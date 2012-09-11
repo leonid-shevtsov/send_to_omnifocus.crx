b017ff2a7bc5e19d554d2470d104ec6ff448cd8f
Guy Bolton King <guy@waftex.com>
2012-09-11 12:37:24 +0100

    Made it possible to set template used for OmniFocus subject

--- background.js
+++ background.js
@@ -1,5 +1,7 @@
 // Called when the user clicks on the browser action.
 chrome.browserAction.onClicked.addListener(function(tab) {
-  chrome.tabs.executeScript(tab.tabId, {file: "send_to_omnifocus.js"});
+  chrome.tabs.executeScript(tab.tabId, {file: "options.js"}, function() {
+    chrome.tabs.executeScript(tab.tabId, {file: "send_to_omnifocus.js"});
+  });
   return true;
 });
