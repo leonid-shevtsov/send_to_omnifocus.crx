fa03612ee994042731887137f550e54d805db755
Leonid Shevtsov <leonid@shevtsov.me>
2013-05-23 09:11:44 +0300

    compatibility with pages that use a strict XHTML schema

--- send_to_omnifocus.js
+++ send_to_omnifocus.js
@@ -9,6 +9,17 @@
 
   var url = 'omnifocus:///add?name='+encodeURIComponent(name)+'&note='+encodeURIComponent(note);
 
-  // window.location = url does not work repetitively because of http://code.google.com/p/chromium/issues/detail?id=104853
-  document.body.insertAdjacentHTML('afterEnd', '<iframe src="'+url+'" style="display:none">');
+  try {
+    // window.location = url does not work repetitively because of http://code.google.com/p/chromium/issues/detail?id=104853
+    document.body.insertAdjacentHTML('afterEnd', '<iframe src="'+url+'" style="display:none">');
+  } catch (e) {
+    if (e instanceof DOMException) {
+      // If the page uses a strict XHTML schema, the iframe method does not work
+      // because the 'omnifocus' URL protocol does not pass validation. Fall back to location method if that is the case.
+      // See http://jblevins.org/projects/deft/ for an example
+      window.location = url;
+    } else {
+      throw e;
+    }
+  }
 })();
